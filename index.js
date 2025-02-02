519b0dc050f743e9e2140518097abab4a5a5a3a3
Aamirnihanms <aamirnihan200@gmail.com>
2025-02-02 21:59:25 +0530

    Updated backend logic again

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://famysas.netlify.app",
     credentials: true,
 }))
 
