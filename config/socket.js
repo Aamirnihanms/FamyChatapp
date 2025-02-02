621e7059cd4d451163acd7a96b6c36eea089df78
Aamirnihanms <aamirnihan200@gmail.com>
2025-02-02 22:03:51 +0530

    resolved cors error

--- config/socket.js
+++ config/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://famysas.netlify.app"],
+    origin: ["https://famysas.netlify.app"],
   },
 });
 
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://famysas.netlify.app",
+    origin: "https://famysas.netlify.app",
     credentials: true,
 }))
 
