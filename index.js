00247ff9522d1f13f12f68d82a48ff78a6bc0b2a
Aamirnihanms <aamirnihan200@gmail.com>
2025-02-02 21:09:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {connectDb }from "./config/db.js"
+import dotenv from "dotenv"
+import authRoutes from "./routes/authRoutes.js"
+import messageRoutes from "./routes/messageRoutes.js"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import { app , server } from "./config/socket.js";
+
+dotenv.config()
+
+
+
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Server running successfully");
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    connectDb()
+});
